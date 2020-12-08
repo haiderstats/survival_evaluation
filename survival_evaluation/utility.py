7b6ca3eeaecb5db0a89df0ae98ad33f025887430
Humza <humza@haiderstats.com>
2020-12-07 19:30:21 -0600

    move km to utility

--- survival_evaluation/utility.py
+++ survival_evaluation/utility.py
@@ -1,3 +1,5 @@
+from dataclasses import InitVar, dataclass, field
+
 import numpy as np  # type: ignore
 
 from survival_evaluation.types import NumericArrayLike
@@ -32,3 +34,83 @@ def validate_size(
     )
     if not same_size:
         raise ValueError("All three inputs must be of the same shape.")
+
+
+@dataclass
+class KaplanMeier:
+    event_times: InitVar[np.array]
+    event_indicators: InitVar[np.array]
+    survival_times: np.array = field(init=False)
+    survival_probabilities: np.array = field(init=False)
+
+    def __post_init__(self, event_times, event_indicators):
+        index = np.lexsort((event_indicators, event_times))
+        unique_times = np.unique(event_times[index], return_counts=True)
+        self.survival_times = unique_times[0]
+        population_count = np.flip(np.flip(unique_times[1]).cumsum())
+
+        event_counter = np.append(0, unique_times[1].cumsum()[:-1])
+        event_ind = list()
+        for i in range(np.size(event_counter[:-1])):
+            event_ind.append(event_counter[i])
+            event_ind.append(event_counter[i + 1])
+        event_ind.append(event_counter[-1])
+        event_ind.append(len(event_indicators))
+        events = np.add.reduceat(np.append(event_indicators[index], 0), event_ind)[::2]
+
+        self.survival_probabilities = np.empty(population_count.size)
+        survival_probability = 1
+        counter = 0
+        for population, event_num in zip(population_count, events):
+            survival_probability *= 1 - event_num / population
+            self.survival_probabilities[counter] = survival_probability
+            counter += 1
+
+    def predict(self, prediction_times: np.array):
+        probability_index = np.digitize(prediction_times, self.survival_times)
+        probability_index = np.where(
+            probability_index == self.survival_times.size + 1,
+            probability_index - 1,
+            probability_index,
+        )
+        probabilities = np.append(1, self.survival_probabilities)[probability_index]
+
+        return probabilities
+
+
+@dataclass
+class KaplanMeierArea:
+    km_model: KaplanMeier
+    area_times: np.array = field(init=False)
+    area_probabilities: np.array = field(init=False)
+    area: np.array = field(init=False)
+
+    def __post_init__(self):
+        area_probabilities = np.append(1, self.km_model.survival_probabilities)
+        area_times = np.append(0, self.km_model.survival_times)
+        if self.km_model.survival_probabilities[-1] != 0:
+            slope = (area_probabilities[-1] - 1) / area_times[-1]
+            zero_survival = -1 / slope
+            area_times = np.append(area_times, zero_survival)
+            area_probabilities = np.append(area_probabilities, 0)
+
+        area_diff = np.diff(area_times, 1)
+        area = np.flip(np.flip(area_diff * area_probabilities[0:-1]).cumsum())
+
+        self.area_times = np.append(area_times, np.inf)
+        self.area_probabilities = area_probabilities
+        self.area = np.append(area, 0)
+
+    def best_guess(self, censor_times: np.array):
+        surv_prob = self.km_model.predict(censor_times)
+        censor_indexes = np.digitize(censor_times, self.area_times)
+        censor_indexes = np.where(
+            censor_indexes == self.area_times.size + 1,
+            censor_indexes - 1,
+            censor_indexes,
+        )
+        censor_area = (
+            self.area_times[censor_indexes] - censor_times
+        ) * self.area_probabilities[censor_indexes - 1]
+        censor_area += self.area[censor_indexes]
+        return censor_times + censor_area / surv_prob
