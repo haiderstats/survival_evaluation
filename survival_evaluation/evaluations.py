39706157e0d23cb2b1dc61eaa1f7ec2fb2b0e238
Humza <humza@haiderstats.com>
2020-12-07 20:05:00 -0600

    move kma and km to utility and get first cut of margin

--- survival_evaluation/evaluations.py
+++ survival_evaluation/evaluations.py
@@ -1,16 +1,24 @@
-from dataclasses import InitVar, dataclass, field
+from typing import Optional
 
 import numpy as np  # type: ignore
 
 from survival_evaluation.types import NumericArrayLike
-from survival_evaluation.utility import check_indicators, to_array, validate_size
+from survival_evaluation.utility import (
+    KaplanMeierArea,
+    check_indicators,
+    to_array,
+    validate_size,
+)
 
 
+# pylint: disable=too-many-arguments
 def l1(
     event_times: NumericArrayLike,
     event_indicators: NumericArrayLike,
     predictions: NumericArrayLike,
-    l1_type: str = "margin",
+    training_event_times: Optional[NumericArrayLike],
+    training_event_indicators: Optional[NumericArrayLike],
+    l1_type: str = "hinge",
 ) -> float:
 
     check_indicators(event_indicators)
@@ -23,88 +31,33 @@ def l1(
     if l1_type == "hinge":
         scores = event_times - predictions
         scores[~event_indicators] = np.maximum(scores[~event_indicators], 0)
-        return np.mean(np.abs(scores))
-    return -1.0
 
+    elif l1_type == "margin":
+        if training_event_times is None or training_event_indicators is None:
+            raise ValueError(
+                "If 'margin' is chosen, training set values must be included."
+            )
 
-@dataclass
-class KaplanMeier:
-    event_times: InitVar[np.array]
-    event_indicators: InitVar[np.array]
-    survival_times: np.array = field(init=False)
-    survival_probabilities: np.array = field(init=False)
+        training_event_times = to_array(training_event_times)
+        training_event_indicators = to_array(training_event_indicators, to_boolean=True)
 
-    def __post_init__(self, event_times, event_indicators):
-        index = np.lexsort((event_indicators, event_times))
-        unique_times = np.unique(event_times[index], return_counts=True)
-        self.survival_times = unique_times[0]
-        population_count = np.flip(np.flip(unique_times[1]).cumsum())
+        km_model = KaplanMeierArea(training_event_times, training_event_indicators)
+        censor_times = event_times[~event_indicators]
+        weights = 1 - km_model.predict(censor_times)
+        best_guesses = km_model.best_guess(censor_times)
 
-        event_counter = np.append(0, unique_times[1].cumsum()[:-1])
-        event_ind = list()
-        for i in range(np.size(event_counter[:-1])):
-            event_ind.append(event_counter[i])
-            event_ind.append(event_counter[i + 1])
-        event_ind.append(event_counter[-1])
-        event_ind.append(len(event_indicators))
-        events = np.add.reduceat(np.append(event_indicators[index], 0), event_ind)[::2]
-
-        self.survival_probabilities = np.empty(population_count.size)
-        survival_probability = 1
-        counter = 0
-        for population, event_num in zip(population_count, events):
-            survival_probability *= 1 - event_num / population
-            self.survival_probabilities[counter] = survival_probability
-            counter += 1
-
-    def predict(self, prediction_times: np.array):
-        probability_index = np.digitize(prediction_times, self.survival_times)
-        probability_index = np.where(
-            probability_index == self.survival_times.size + 1,
-            probability_index - 1,
-            probability_index,
+        scores = np.empty(predictions.size)
+        scores[event_indicators] = (
+            event_times[event_indicators] - predictions[event_indicators]
         )
-        probabilities = np.append(1, self.survival_probabilities)[probability_index]
-
-        return probabilities
-
-
-@dataclass
-class KaplanMeierArea:
-    km_model: KaplanMeier
-    area_times: np.array = field(init=False)
-    area_probabilities: np.array = field(init=False)
-    area: np.array = field(init=False)
-
-    def __post_init__(self):
-        area_probabilities = np.append(1, self.km_model.survival_probabilities)
-        area_times = np.append(0, self.km_model.survival_times)
-        if self.km_model.survival_probabilities[-1] != 0:
-            slope = (area_probabilities[-1] - 1) / area_times[-1]
-            zero_survival = -1 / slope
-            area_times = np.append(area_times, zero_survival)
-            area_probabilities = np.append(area_probabilities, 0)
-
-        area_diff = np.diff(area_times, 1)
-        area = np.flip(np.flip(area_diff * area_probabilities[0:-1]).cumsum())
-
-        self.area_times = np.append(area_times, np.inf)
-        self.area_probabilities = area_probabilities
-        self.area = np.append(area, 0)
-
-    def best_guess(self, censor_times: np.array):
-        surv_prob = self.km_model.predict(censor_times)
-        censor_indexes = np.digitize(censor_times, self.area_times)
-        censor_indexes = np.where(
-            censor_indexes == self.area_times.size + 1,
-            censor_indexes - 1,
-            censor_indexes,
+        scores[~event_indicators] = weights * (
+            best_guesses - predictions[~event_indicators]
         )
-        censor_area = (
-            self.area_times[censor_indexes] - censor_times
-        ) * self.area_probabilities[censor_indexes - 1]
-        censor_area += self.area[censor_indexes]
-        return censor_times + censor_area / surv_prob
+
+    else:
+        raise ValueError("L1 type must be either 'hinge' or 'margin'.")
+
+    return np.mean(np.abs(scores))
 
 
 # import time
@@ -113,9 +66,9 @@ class KaplanMeierArea:
 #     start = time.time()
 #     event_times = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 #     event_indicators = np.array([1, 1, 1, 0, 0, 1, 0, 0, 0, 1])
-#     km = KaplanMeier(event_times, event_indicators)
-#     print(km.survival_times)
-#     kma = KaplanMeierArea(km)
+#     kma = KaplanMeierArea(event_times, event_indicators)
+#     print(kma.survival_times)
+#     # kma = KaplanMeierArea(km)
 #     print(kma.area)
 #     print(kma.area_times)
 #     print(kma.area_probabilities)
